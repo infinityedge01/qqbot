59bf36e76c4772196bfd4b3918dfe1225cac4f2c
infedg <309173017@qq.com>
2020-06-15 16:20:47 +0800

    add delete msg

--- plugins/setu/__init__.py
+++ plugins/setu/__init__.py
@@ -11,10 +11,10 @@ async def setu(session: CommandSession):
         has_perm = await perm.check_permission(session.bot, session.event, perm.SUPERUSER)
         if has_perm:
             msg1 = message.MessageSegment.image('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592212494831&di=ee6127d25949ab52d82402d2309a8537&imgtype=0&src=http%3A%2F%2Fb.hiphotos.baidu.com%2Fzhidao%2Fwh%253D450%252C600%2Fsign%3D14f304ca50da81cb4eb38bc96756fc20%2Fae51f3deb48f8c542d7329113b292df5e0fe7f68.jpg')
-            msg_id = await session.send(msg1)
-            log.logger.debug(str(msg_id))
+            msg_data = await session.send(msg1)
+            log.logger.debug(str(msg_data))
             await asyncio.sleep(10)
-            get_bot().delete_msg(message_id = msg_id, self_id = session.event.self_id)
+            get_bot().delete_msg(message_id = msg_data['message_id'], self_id = session.event.self_id)
         else:
             msg1 = message.MessageSegment.text('我们可以通过“色图”来表示所有自然界之色，国际照明学会规定分别用x、y、z来表示红、绿、蓝三原色之间的百分比。由于是百分比，三者相加必须等于1，故色调在色图中只需用x、y两值即可。将光谱色中各段波长所引起的色调感觉在x、y平面上做成图标时，即得色图。')
             await session.send(msg1)      
